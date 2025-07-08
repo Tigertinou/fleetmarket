f7556eb51951a14dd0a90656ab532beddd7163b1
QUBA <q.b@mouseonmove.com>
2025-07-08 11:15:56 +0200

    Tinou

--- resources/views/pages/vehicles/configurator.blade.php
+++ resources/views/pages/vehicles/configurator.blade.php
@@ -65,8 +65,12 @@ $motorSelected = $motors->first()['versionUrlCode'] ?? null;
         coverImage : '{{ $submodeColors['data']['external'][0]['colorImage']['image800'] ?? '' }}',
         coverLabel : '',
         init() {
-            this.$el.querySelector('.vcolors-image').style.backgroundImage = 'url(' + this.coverImage + ')';
-            this.$el.querySelector('.vcolors-label').innerHTML = this.coverLabel;
+            this.$el.querySelectorAll('.vcolors-image').forEach( (el) => {
+                el.style.backgroundImage = 'url(' + this.coverImage + ')';
+            });
+            this.$el.querySelectorAll('.vcolors-label').forEach( (el) => {
+                el.innerHTML = this.coverLabel;
+            });
         },
         selectVersion(versionId) {
             this.versionSelected = versionId;
@@ -74,21 +78,30 @@ $motorSelected = $motors->first()['versionUrlCode'] ?? null;
             {{-- window.selectVersion(versionId); --}}
         },
     }">
-
-        <div class="bg-white border-b border-gray-200">
-            <div class="max-w-screen-xl mx-auto overflow-auto scrollbar-hide" style="-ms-overflow-style: none; scrollbar-width: none;">
-                <div class="flex max-w-full gap-6 px-6 text-sm font-semibold md:px-4 flex-nowrap">
-                    <div class="pt-6 pb-3 tab-item" :class="activeTab=='models' ? 'border-b-4 border-black' : ''">MODÈLE</div>
-                    <div class="pt-6 pb-3 tab-item">FINITIONS</div>
-                    <div class="pt-6 pb-3 tab-item">MOTEURS</div>
-                    <div class="pt-6 pb-3 tab-item">EXTÉRIEUR</div>
-                    <div class="pt-6 pb-3 tab-item">INTÉRIEUR</div>
-                    <div class="pt-6 pb-3 tab-item">JANTES</div>
-                    <div class="pt-6 pb-3 tab-item">PACKS</div>
-                    <div class="pt-6 pb-3 tab-item">OPTIONS</div>
-                    <div class="pt-6 pb-3 tab-item">ACCESSOIRES</div>
-                    <div class="pt-6 pb-3 tab-item">RÉCAPITULATIF</div>
-                    <div>&nbsp;</div>
+        <div class="sticky top-0 z-30 bg-white">
+            <div class="md:hidden p-4 pb-0">
+                <div class="relative flex flex-col items-center w-full p-4 pb-6 overflow-hidden border-gray-200 justify-items-stretch border-1 rounded-xl">
+                    {{-- <img src="{{ $make['logo'] }}" class="absolute w-10 md:w-24 md:order-1 right-4 top-4" alt="{{ $vehicle['model']['makeName'] }} logo"> --}}
+                    <div class="self-center w-full max-w-lg bg-contain vcolors-image aspect-video"></div>
+                    <div class="absolute text-xs font-normal vcolors-label top-4 left-4"></div>
+                    {{-- <img src="{{ $submodeColors['data']['external'][0]['colorImage']['image800'] }}" class="object-cover w-full h-full"> --}}
+                </div>
+            </div>
+            <div class="bg-white border-b border-gray-200">
+                <div class="max-w-screen-xl mx-auto overflow-auto scrollbar-hide" style="-ms-overflow-style: none; scrollbar-width: none;">
+                    <div class="flex max-w-full gap-6 px-6 text-sm font-semibold md:px-4 flex-nowrap">
+                        <div class="pt-6 pb-3 tab-item" :class="activeTab=='models' ? 'border-b-4 border-black' : ''">MODÈLE</div>
+                        <div class="pt-6 pb-3 tab-item">FINITIONS</div>
+                        <div class="pt-6 pb-3 tab-item">MOTEURS</div>
+                        <div class="pt-6 pb-3 tab-item">EXTÉRIEUR</div>
+                        <div class="pt-6 pb-3 tab-item">INTÉRIEUR</div>
+                        <div class="pt-6 pb-3 tab-item">JANTES</div>
+                        <div class="pt-6 pb-3 tab-item">PACKS</div>
+                        <div class="pt-6 pb-3 tab-item">OPTIONS</div>
+                        <div class="pt-6 pb-3 tab-item">ACCESSOIRES</div>
+                        <div class="pt-6 pb-3 tab-item">RÉCAPITULATIF</div>
+                        <div>&nbsp;</div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -116,7 +129,7 @@ $motorSelected = $motors->first()['versionUrlCode'] ?? null;
                     <img src="{{ $make['logo'] }}" class="self-start order-2 w-20 md:w-24 md:order-3" alt="{{ $vehicle['model']['makeName'] }} logo">
                 </div>
 
-                <div>
+                <div class="hidden md:block">
                     <div class="relative flex flex-col items-center w-full p-4 pb-6 overflow-hidden border-gray-200 justify-items-stretch border-1 rounded-xl">
                         {{-- <img src="{{ $make['logo'] }}" class="absolute w-10 md:w-24 md:order-1 right-4 top-4" alt="{{ $vehicle['model']['makeName'] }} logo"> --}}
                         <div class="self-center w-full max-w-lg bg-contain vcolors-image aspect-video"></div>
@@ -126,7 +139,7 @@ $motorSelected = $motors->first()['versionUrlCode'] ?? null;
                 </div>
             </div>
             {{-- *************************************** RIGHT *************************************** --}}
-            <div class="py-6 border-gray-200 md:px-4 md:border-l md:overflow-visible z-100 md:w-lg md:relative md:h-auto md:z-5">
+            <div class="py-6 border-gray-200 md:px-4 md:border-l md:overflow-visible md:w-lg md:relative md:h-auto md:z-5">
                 <h2 class="text-2xl">Configuration</h2>
                 {{-- ************* FINITIONS ************* --}}
                 <h3 class="text-lg font-semibold">Finitions</h3>
@@ -196,12 +209,12 @@ $motorSelected = $motors->first()['versionUrlCode'] ?? null;
                                 <x-forms.elements.radio name="inp_motor" value="{{ $motor['versionUrlCode'] }}" @change="change($event.target)" size="md" class="pt-0.5 -ml-2 "/>
                                 <div class="flex w-full gap-1 leading-5 justify-stretch">
                                     <div class="flex-1">
-                                        <b class="font-semibold uppercase">{{ $motor['versionName'] }}</b>
+                                        <b class="font-semibold">{{ $motor['versionName'] }}</b>
                                         <br><small><b class="uppercase">{{ $motor['fuelType']}}</b></small>
                                     </div>
                                     <div class="justify-end text-right">
                                         <span class="flex-1 text-xs whitespace-nowrap">à partir de</span><br>
-                                        <span class="text-xs whitespace-nowrap"><span class="text-base font-extrabold md:text-base">{{ number_format($finition['price'], 0, ',', '.') . ' €' }}</span></span>
+                                        <span class="text-xs whitespace-nowrap"><span class="text-base font-extrabold md:text-base">{{ number_format($motor['price'], 0, ',', '.') . ' €' }}</span></span>
                                     </div>
                                 </div>
                             </div>
@@ -210,6 +223,30 @@ $motorSelected = $motors->first()['versionUrlCode'] ?? null;
                 </div>
 
             </div>
+        </div>
+
+        <div class="sticky bottom-0 z-30 bg-gray-100">
+            <div class="flex flex-col max-w-screen-xl mx-auto md:flex-row items-center md:px-4">
+                <div class="flex-1 py-4 leading-5 w-full px-4 md:px-0">
+                    <span class="font-bold">{{ $vehicle['model']['makeName'] }}</span>
+                    <span class="font-normal">{{ $vehicle['model']['submodelCommercialName'] ?? $vehicle['model']['modelName'] }}</span><br>
+                    <small>35 TFSI 150 MHEV S Tronic Design</small>
+                </div>
+                <div class="py-3 md:w-lg md:relative flex items-center gap-4 bg-gray-200 px-4 w-full">
+                    <div class="flex-1 leading-5">
+                        <span class="text-xs font-normal">à partir de</span><br>
+                        <span class="text-2xl font-bold">40.000 €</span><small> TTC*</small>
+                    </div>
+                    <div class="w-1/2">
+                        <x-utils.button label="Continuer" r-icon="icon-chevron-right" class="w-full max-w-sm"></x-utils.button>
+                    </div>
+                </div>
+            </div>
+            {{--
+            <div class="max-w-screen-xl px-4 py-6 mx-auto text-sm text-gray-500">
+                <p class="text-xs">* Prix TTC, hors options et accessoires. Les prix peuvent varier en fonction des options sélectionnées et des promotions en cours.</p>
+                <p class="text-xs">Les informations présentées sont à titre indicatif et peuvent être modifiées sans préavis.</p>
+            </div>--}}
         </div>
 
     </div>
